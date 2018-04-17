af2fbd0299a666326f354a27944e95cfd0258809
Simon Böhm <simonboehm@mailbox.org>
2018-04-18 00:21:24 +0100

    Get checker working, add iteration parameter

--- theorem_breaker.py
+++ theorem_breaker.py
@@ -9,23 +9,18 @@ def xor(arr):
     return reduce(lambda res, el: res != el, arr)
 
 
+# returns array containing only the elements at the specified indices
 def filter_by_index(arr, indices):
     return [x for x, i in zip(arr, range(len(arr))) if i in indices]
 
 
-# setting an arbitrary random key
-long_term_key = {
-    "alpha": 23,
-    "D": [None, 0, 4, 20, 8, 32, 12, 24, 16, 28],
-    "P": [
-        None, 8, 20, 33, 31, 17, 16, 5, 19, 9, 2, 18, 28, 24, 1, 21, 11, 3, 25,
-        4, 36, 12, 22, 15, 29, 23, 32, 6
-    ]
-}
 random.seed(123)
-IV = [int(i) for i in format(random.getrandbits(61), '0>61b')]
-st_key = [int(s) for s in format(random.getrandbits(240), '0>240b')]
-s1, s2 = st_key[:120], st_key[120:]
+NUM_ITER = 5
+
+long_term_key = {"alpha": 123, "D": [], "P": []}
+IV = [0 for _ in range(61)]
+s1 = [0 for _ in range(120)]
+s2 = [0 for _ in range(120)]
 
 theorems = parse_file()[0]
 broken = 0
@@ -33,13 +28,26 @@ for theorem in theorems:
     long_term_key["D"] = [None] + theorem["D"]
     long_term_key["P"] = [None] + theorem["P"]
 
+    # setup the cypher
     enc = ARGON(long_term_key, IV, s1, s2)
 
-    input_xor = xor(filter_by_index(enc.settings["u0"], theorem["lin_prop"]))
-    output_xor = xor(
-        filter_by_index(enc.u(theorem["rounds"]), theorem["lin_prop"]))
+    for _ in range(NUM_ITER):
+        # generate some new input
+        u = [None] + [int(i) for i in format(random.getrandbits(36), '0>36b')]
 
-    if input_xor != output_xor:
-        broken += 1
+        # feed input to cypher
+        enc.settings["u0"] = u
+        enc.reset()
 
-print "broken:", broken, "num", len(theorems)
+        input_xor = xor(filter_by_index(u, theorem["lin_prop"]))
+
+        # run the cypher
+        output = enc.u(theorem["rounds"])
+        output_xor = xor(filter_by_index(output, theorem["lin_prop"]))
+
+        if input_xor != output_xor:
+            broken += 1
+            # print theorem
+            break
+
+print "broken:", broken, "num:", len(theorems)
